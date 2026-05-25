79a67dce38da1f7cffe8727aa93173916923ee0b
Lynx <lynx@ilynxcat.me>
2026-05-24 19:57:10 -0500

    add window & methods

--- oct.m
+++ oct.m
@@ -1,18 +1,61 @@
 #import <AppKit/AppKit.h>
+#import <Foundation/Foundation.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
+@end
+
+@interface AppDelegate ()
+- (void)setupMenu;
+- (void)setupWindow;
+- (void)showAlert;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
+  [self setupMenu];
+  [self setupWindow];
+  [self showAlert];
+}
+
+- (void)setupMenu {
+  NSMenu *menuBar = [[NSMenu alloc] init];
+  NSMenuItem *appItem = [[NSMenuItem alloc] init];
+
+  [menuBar addItem:appItem];
+  [NSApp setMainMenu:menuBar];
+
+  NSMenu *appMenu = [[NSMenu alloc] init];
+  [appMenu addItemWithTitle:@"Quit OCT"
+                     action:@selector(terminate:)
+              keyEquivalent:@"q"];
+  [appItem setSubmenu:appMenu];
+}
+
+- (void)setupWindow {
+  NSWindow *window = [[NSWindow alloc]
+      initWithContentRect:NSMakeRect(0, 0, 420, 640)
+                styleMask:NSWindowStyleMaskClosable |
+                          NSWindowStyleMaskResizable | NSWindowStyleMaskTitled
+                  backing:NSBackingStoreBuffered
+                    defer:NO];
+
+  [window setTitle:@"oct!"];
+  [window makeKeyAndOrderFront:nil];
+  [window center];
+}
+
+- (void)showAlert {
   NSAlert *alert = [[NSAlert alloc] init];
   alert.messageText = @"Hello, world!";
 
   [alert addButtonWithTitle:@"Dismiss"];
   [alert runModal];
+}
 
-  [NSApp terminate:nil];
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:
+    (NSApplication *)sender {
+  return YES;
 }
 
 @end
@@ -22,11 +65,11 @@ AppDelegate *delegate;
 int main() {
   [NSApplication sharedApplication];
 
-  delegate = [[AppDelegate alloc] init];
-  NSApp.delegate = delegate;
-
   [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
   [NSApp activateIgnoringOtherApps:YES];
+
+  delegate = [[AppDelegate alloc] init];
+  NSApp.delegate = delegate;
 
   [NSApp run];
   return 0;
